3ca8ca032cb695b2b6f2b789adc822f5741db91f
Dominic Magnifico <dominicmagnifico@gmail.com>
2022-02-21 12:53:18 -0700

    Add SVG components for use thrgouhout the site.

---
+++ frontend/components/SVG/Menu.js
@@ -0,0 +1,5 @@
+export const Menu = ({ width, height, stroke = '#000' }) => (
+  <svg fill="none" width={width} height={height} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 16">
+    <path stroke={stroke} d="M0 1h19M0 8h25M0 15h19"/>
+  </svg>
+)
