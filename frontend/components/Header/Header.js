d8bfa5a55b71a4f98175abe3a516cf34dfd40d58
Dominic Magnifico <dominicmagnifico@gmail.com>
2022-02-22 14:36:02 -0700

    Add basic animations for showing and hiding the nav menu.

--- frontend/components/Header/Header.js
+++ frontend/components/Header/Header.js
@@ -1,5 +1,6 @@
+import { AnimatePresence, motion } from 'framer-motion';
 import Link from 'next/link';
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 import { Social } from '../Social/Social';
 
@@ -7,10 +8,10 @@ import { Logo } from '../SVG/Logo';
 import { Menu } from '../SVG/Menu';
 
 import styles from './Header.module.css';
+import { container, item, navContainer } from './Header.motion';
 
 export const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
-
   return (
     <div className={styles.header}>
       <div className="container">
@@ -28,37 +29,52 @@ export const Header = () => {
           <Menu width={25} height={14} />
         </button>
       </div>
-      {menuOpen && (
-        <div id="menu" className={styles['menu-container']}>
-          <div className="container">
-            <ul className={styles['primary-nav']}>
-              <li>
-                <Link href="/"><a className="h1">Ryukyu Whiskey</a></Link>
-              </li>
-              <li>
-                <Link href="/"><a className="h1">Our Craft</a></Link>
-              </li>
-              <li>
-                <Link href="/"><a className="h1">Blog</a></Link>
-              </li>
-              <li>
-                <Link href="/"><a>Contact</a></Link>
-              </li>
-              <li>
-                <Link href="/"><a>Where to buy</a></Link>
-              </li>
-              <li>
-                <Link href="/"><a>Order online</a></Link>
-              </li>
-            </ul>
-            <div className={styles['menu-footer']}>
-              <p>&copy; {new Date().getFullYear()} Hayashi</p>
-              <Link href="/"><a>Privacy Policy</a></Link>
-              <Social />
+      <AnimatePresence>
+        {menuOpen && (
+          <motion.div  
+            variants={navContainer}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            id="menu"
+            className={styles['menu-container']}
+          >
+            <div className="container">
+              <motion.ul
+                variants={container}
+                initial="hidden"
+                animate="show"
+                exit="hidden"
+                className={styles['primary-nav']}
+              >
+                <motion.li variants={item}>
+                  <Link href="/"><a className="h1">Ryukyu Whiskey</a></Link>
+                </motion.li>
+                <motion.li variants={item}>
+                  <Link href="/"><a className="h1">Our Craft</a></Link>
+                </motion.li>
+                <motion.li variants={item}>
+                  <Link href="/"><a className="h1">Blog</a></Link>
+                </motion.li>
+                <motion.li variants={item}>
+                  <Link href="/"><a>Contact</a></Link>
+                </motion.li>
+                <motion.li variants={item}>
+                  <Link href="/"><a>Where to buy</a></Link>
+                </motion.li>
+                <motion.li variants={item}>
+                  <Link href="/"><a>Order online</a></Link>
+                </motion.li>
+              </motion.ul>
+              <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 1 }} className={styles['menu-footer']}>
+                <p>&copy; {new Date().getFullYear()} Hayashi</p>
+                <Link href="/"><a>Privacy Policy</a></Link>
+                <Social />
+              </motion.div>
             </div>
-          </div>
-        </div>
-      )}
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
