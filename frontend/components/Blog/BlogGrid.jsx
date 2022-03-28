27d6add11508d6543d12b46f5d4b0d70cab70dad
Dominic Magnifico <dominicmagnifico@gmail.com>
2022-03-28 14:22:29 -0600

    Add blog page layout and styles.

---
+++ frontend/components/Blog/BlogGrid.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import { BlogGridItem } from './BlogGridItem';
+
+import styles from './BlogGrid.module.css';
+
+export const BlogGrid = () => {
+  return (
+    <div className={styles['blog-grid']}>
+      {/* @TODO :: Query blog post type */}
+      <BlogGridItem />
+      <BlogGridItem />
+      <BlogGridItem />
+      <BlogGridItem />
+      <BlogGridItem />
+    </div>
+  )
+};
