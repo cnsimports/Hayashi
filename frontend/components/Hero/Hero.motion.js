beb0f1a1323e577584436a1f9e891ed87dc1b00e
Dominic Magnifico <dominicmagnifico@gmail.com>
2022-03-28 13:14:29 -0600

    Add Hero component layout, styles and motion.

---
+++ frontend/components/Hero/Hero.motion.js
@@ -0,0 +1,20 @@
+export const heroMotion = {
+  fade: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      opacity: {
+        duration: 1.3,
+        delay: 2.3
+      },
+      y: {
+        duration: 0.666,
+        delay: 2.3
+      }
+    }
+  },
+  hidden: {
+    opacity: 0,
+    y: 50,
+  }
+}
