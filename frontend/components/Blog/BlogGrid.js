c70ec0a9bdba8bc740e213726f98e0ac6cdad9f6
Dominic Magnifico <dominicmagnifico@gmail.com>
2022-04-11 15:30:49 -0600

    Hook up Strapi to blog grid.

--- frontend/components/Blog/BlogGrid.js
+++ frontend/components/Blog/BlogGrid.js
@@ -2,15 +2,12 @@ import { BlogGridItem } from './BlogGridItem';
 
 import styles from './BlogGrid.module.css';
 
-export const BlogGrid = () => {
+export const BlogGrid = ({ posts }) => {
 	return (
 		<div className={styles['blog-grid']}>
-			{/* @TODO :: Query blog post type */}
-			<BlogGridItem />
-			<BlogGridItem />
-			<BlogGridItem />
-			<BlogGridItem />
-			<BlogGridItem />
+			{posts.data.map((post, i) => (
+				<BlogGridItem key={post.attributes.Title} thumbnail={post.attributes.FeaturedImage} slug={post.attributes.slug} count={`${i + 1}`} title={post.attributes.Title} featuredImage={post.attributes.FeaturedImage} />
+			))}
 		</div>
 	);
 };
--- frontend/components/Blog/BlogGridItem.js
+++ frontend/components/Blog/BlogGridItem.js
@@ -3,24 +3,26 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 import styles from './BlogGrid.module.css';
+import { getStrapiMedia } from '@lib/media';
 
-export const BlogGridItem = ({ count = '1' }) => {
+export const BlogGridItem = ({ title, thumbnail, count, slug }) => {
 	const padCount = count.toString().padStart(3, '0');
+	const { url, alternativeText } = thumbnail.data.attributes;
 
 	return (
-		<Link href="/">
+		<Link href={`/blog/${slug}`}>
 			<a className={styles['blog-grid-item']}>
 				<p>{padCount}</p>
 				<div className="img">
 					<Image
-						alt=""
-						src="https://source.unsplash.com/collection/928423/480x480"
+						alt={alternativeText}
+						src={getStrapiMedia(url)}
 						layout="responsive"
 						width={600}
 						height={656}
 					/>
 				</div>
-				<h4>Donec ullamcorper nulla non metus auctor fringilla.</h4>
+				<h4>{title}</h4>
 			</a>
 		</Link>
 	);
