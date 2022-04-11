bfaed75083926cf84abdb48a4a566f84d25d6864
Dominic Magnifico <dominicmagnifico@gmail.com>
2022-04-11 15:50:08 -0600

    Add post types, and single page content.

---
+++ backend/src/api/ryukyu-whiskey/controllers/ryukyu-whiskey.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  ryukyu-whiskey controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::ryukyu-whiskey.ryukyu-whiskey');
