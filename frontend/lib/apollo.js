dbe74334d0a30b589391ae07524678dfe3697a19
Dominic Magnifico <dominicmagnifico@gmail.com>
2022-04-12 12:19:56 -0600

    Update apollo API url to use strapi.

--- frontend/lib/apollo.js
+++ frontend/lib/apollo.js
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:1337';
+const API_URL = process.env.NEXT_PUBLIC_STRAPI_API_URL || 'http://localhost:1337';
 
 const client = new ApolloClient({
 	uri: `${API_URL}/graphql`,
