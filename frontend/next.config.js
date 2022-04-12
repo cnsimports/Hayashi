2247d9323c05d3a8ace7e1bbb8b217c374f644a0
Dominic Magnifico <dominicmagnifico@gmail.com>
2022-04-12 12:19:37 -0600

    Add heroku app as image source.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	reactStrictMode: true,
 	images: {
-		domains: ['source.unsplash.com', 'localhost'],
+		domains: ['source.unsplash.com', 'localhost', 'hayashiwhisky.herokuapp.com'],
 	},
 };
