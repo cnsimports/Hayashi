2ed3957b089f27b8fca318a8725b2e322caf485f
Dominic Magnifico <dominicmagnifico@gmail.com>
2022-04-21 16:18:52 -0600

    Don't use lazy animations in hopes that it fixes the weird issues on vercel.

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,27 +1,27 @@
 import { createContext, useEffect, useState } from 'react';
-import PropTypes from 'prop-types';
-// import App from 'next/app';
+import { AnimatePresence, motion } from 'framer-motion';
+import App from 'next/app';
+import Image from 'next/image';
 import Head from 'next/head';
-import { AnimatePresence, LazyMotion, m, domAnimation, motion } from 'framer-motion';
+import PropTypes from 'prop-types';
 
 import { Header } from '@components/Header/Header';
 import { Footer } from '@components/Footer/Footer';
 import Gate from '@components/Gate/Gate';
+import { Kanji } from '@components/SVG/Kanji';
+
+import { getStrapiMedia } from '@lib/media';
+import { fetchAPI } from '@lib/api';
 
 import styles from '@styles/pageTransition/pageTransition.module.css';
 
-// import { getStrapiMedia } from '@lib/media';
-// import { fetchAPI } from '@lib/api';
-
 import '@styles/globals.css';
-import { Kanji } from '@components/SVG/Kanji';
-import Image from 'next/image';
 
 export const GlobalContext = createContext({});
 
 function MyApp({ Component, pageProps, router }) {
 	const { route } = router;
-	// const { global } = pageProps;
+	const { global } = pageProps;
 	const [isLegal, setIsLegal] = useState('');
 
 	useEffect(() => {
@@ -37,23 +37,24 @@ function MyApp({ Component, pageProps, router }) {
 	return (
 		<>
 			<Head>
-				{/* <link rel="shortcut icon" href={getStrapiMedia(global.attributes.favicon.data.attributes.url)} /> */}
+				<link rel="shortcut icon" href={getStrapiMedia(global.attributes.favicon.data.attributes.url)} />
 				<link rel="stylesheet" href="https://use.typekit.net/wzt1kkc.css" />
 			</Head>
-			{/* <GlobalContext.Provider value={global.attributes}> */}
-			{isLegal === 'true' && <Header initial={false} animate={{ opacity: 1 }} transition={{ delay: 1, duration: 1 }} />}
-			{isLegal === 'true' ? (
-				<LazyMotion features={domAnimation}>
+			<GlobalContext.Provider value={global.attributes}>
+				{isLegal === 'true' && (
+					<Header initial={false} animate={{ opacity: 1 }} transition={{ delay: 1, duration: 1 }} />
+				)}
+				{isLegal === 'true' ? (
 					<AnimatePresence exitBeforeEnter>
-						<m.div
+						<motion.div
 							initial={{ opacity: 0 }}
 							animate={{ opacity: 1 }}
 							transition={{ delay: 1.5 }}
 							key={route.concat('fade')}
 						>
 							<Component {...pageProps} />
-						</m.div>
-						<m.div
+						</motion.div>
+						<motion.div
 							key={route.concat('slide-in')}
 							className={styles['slide-in']}
 							initial={{ y: '100%' }}
@@ -66,8 +67,8 @@ function MyApp({ Component, pageProps, router }) {
 							<div className={styles.kanji}>
 								<Image alt="" src="/images/gold-foil.jpeg" priority layout="intrinsic" width={271} height={231} />
 							</div>
-						</m.div>
-						<m.div
+						</motion.div>
+						<motion.div
 							key={route.concat('slide-out')}
 							className={styles['slide-out']}
 							initial={{ opacity: 1 }}
@@ -80,43 +81,42 @@ function MyApp({ Component, pageProps, router }) {
 							<div className={styles.kanji}>
 								<Image alt="" src="/images/gold-foil.jpeg" priority layout="intrinsic" width={271} height={231} />
 							</div>
-						</m.div>
+						</motion.div>
 					</AnimatePresence>
-				</LazyMotion>
-			) : (
-				<MotionGate
-					key="gate"
-					initial={{ opacity: 0 }}
-					animate={{ opacity: 1 }}
-					exit={{ opacity: 0 }}
-					transition={{ duration: 1 }}
-					setIsLegal={setIsLegal}
-					isLegal={isLegal}
-				/>
-			)}
-			{isLegal === 'true' && <Footer initial={false} animate={{ opacity: 1 }} transition={{ delay: 1, duration: 1 }} />}
-			{/* </GlobalContext.Provider> */}
+				) : (
+					<MotionGate
+						key="gate"
+						initial={{ opacity: 0 }}
+						animate={{ opacity: 1 }}
+						exit={{ opacity: 0 }}
+						transition={{ duration: 1 }}
+						setIsLegal={setIsLegal}
+						isLegal={isLegal}
+					/>
+				)}
+				{isLegal === 'true' && (
+					<Footer initial={false} animate={{ opacity: 1 }} transition={{ delay: 1, duration: 1 }} />
+				)}
+			</GlobalContext.Provider>
 		</>
 	);
 }
+
+MyApp.getInitialProps = async (ctx) => {
+	const appProps = await App.getInitialProps(ctx);
+	const globalRes = await fetchAPI('/global', {
+		populate: {
+			favicon: '*',
+		},
+	});
+
+	return { ...appProps, pageProps: { global: globalRes.data } };
+};
 
 MyApp.propTypes = {
 	Component: PropTypes.func,
 	pageProps: PropTypes.object,
 	router: PropTypes.object,
 };
-
-// MyApp.getInitialProps = async (ctx) => {
-// 	const appProps = await App.getInitialProps(ctx);
-// 	// We don't use GraphQL here because by design we cannot pass _all_ global attributes down as context.
-// 	// With that, we'll maintain use of a normal JS fetch call here, and pass the options needed for the favicon.
-// 	const globalRes = await fetchAPI('/global', {
-// 		populate: {
-// 			favicon: '*',
-// 		},
-// 	});
-
-// 	return { ...appProps, pageProps: { global: globalRes.data } };
-// };
 
 export default MyApp;
