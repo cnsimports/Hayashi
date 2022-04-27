26f0e322ffbae1306c0a5547de613d54fd26f684
Dominic Magnifico <dominicmagnifico@gmail.com>
2022-04-27 11:58:17 -0600

    Add conditional to swap markup for images vs videos.

--- frontend/components/CoverImage/CoverImage.js
+++ frontend/components/CoverImage/CoverImage.js
@@ -5,14 +5,21 @@ import PropTypes from 'prop-types';
 import styles from './CoverImage.module.css';
 
 export const CoverImage = ({ CoverImage }) => {
+	const { alternativeText, url, mime } = CoverImage.data.attributes;
 	return (
 		<div className={styles['cover-image']}>
-			<Image
-				priority
-				alt={CoverImage.data.attributes.alternativeText}
-				src={getStrapiMedia(CoverImage.data.attributes.url)}
-				layout="fill"
-			/>
+			{mime.includes('video') ? (
+				<video muted={true} controls={false} autoPlay={true} loop={true}>
+					<source src={getStrapiMedia(url)} />
+				</video>
+			): (
+				<Image
+					priority
+					alt={alternativeText}
+					src={getStrapiMedia(url)}
+					layout="fill"
+				/>
+			)}
 		</div>
 	);
 };
@@ -22,6 +29,7 @@ CoverImage.propTypes = {
 	CoverImage: PropTypes.shape({
 		data: PropTypes.shape({
 			attributes: PropTypes.shape({
+				mime: PropTypes.string,
 				alternativeText: PropTypes.string,
 				url: PropTypes.string.isRequired,
 			}),
