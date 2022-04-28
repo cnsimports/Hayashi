8cf7701bf36b4a9017b6ac7e68700d993b6f2b71
Dominic Magnifico <dominicmagnifico@gmail.com>
2022-04-28 16:27:41 -0600

    Add new reusable components, and a new page collection.

--- backend/src/api/page/services/page.js
+++ backend/src/api/page/services/page.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * our-craft service.
+ * page service.
  */
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::our-craft.our-craft');
+module.exports = createCoreService('api::page.page');
--- backend/src/api/ryukyu-whiskey/routes/ryukyu-whiskey.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * ryukyu-whiskey router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::ryukyu-whiskey.ryukyu-whiskey');
