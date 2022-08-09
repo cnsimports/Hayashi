2366e42f98ec85ab1bef2a3827f00dbad4cb4ba4
Dominic Magnifico <dominicmagnifico@gmail.com>
2022-08-09 14:33:52 -0600

    Make sure apollo doesn't cache stuff.

--- frontend/lib/apollo.js
+++ frontend/lib/apollo.js
@@ -5,6 +5,11 @@ const API_URL = process.env.NEXT_PUBLIC_STRAPI_API_URL || 'http://localhost:1337
 const client = new ApolloClient({
 	uri: `${API_URL}/graphql`,
 	cache: new InMemoryCache(),
+	defaultOptions: {
+		query: {
+			fetchPolicy: 'no-cache',
+		},
+	},
 });
 
 export default client;
