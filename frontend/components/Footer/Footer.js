05f96997e1652add8409b06ffad84a62ac852930
Dominic Magnifico <dominicmagnifico@gmail.com>
2022-08-10 11:59:02 -0600

    Add zoho footer form.

--- frontend/components/Footer/Footer.js
+++ frontend/components/Footer/Footer.js
@@ -32,12 +32,268 @@ export const Footer = () => (
 			</ul>
 
 			<div className={styles.subscribe}>
+				<>
+					{/*Zoho Campaigns Web-Optin Form's Header Code Starts Here*/}
+					<style
+						dangerouslySetInnerHTML={{
+							__html:
+								'\n.quick_form_8_css * {\n    -webkit-box-sizing: border-box !important;\n    -moz-box-sizing: border-box !important;\n    box-sizing: border-box !important;\n    overflow-wrap: break-word\n}\n@media only screen and (max-width: 600px) {.quick_form_8_css[name="SIGNUP_BODY"] { width: 100% !important; min-width: 100% !important; margin: 0px auto !important; padding: 0px !important } .SIGNUP_FLD { width: 90% !important; margin: 10px 5% !important; padding: 0px !important } .SIGNUP_FLD input { margin: 0 !important; border-radius: 25px !important } }\n'
+						}}
+					/>
+					{/*Zoho Campaigns Web-Optin Form's Header Code Ends Here*/}
+					{/*Zoho Campaigns Web-Optin Form Starts Here*/}
+					<div
+						id="sf3z5fb2122fba895524fb95f5eb7bc1e05e8d1492518627420aa5e5dd518c37d62a"
+						data-type="signupform"
+						style={{ opacity: 1 }}
+					>
+						<div id="customForm">
+							<div
+								className="quick_form_8_css"
+								name="SIGNUP_BODY"
+							>
+								<div>
+									<label
+										htmlFor="EMBED_FORM_EMAIL_LABEL"
+										id="SIGNUP_HEADING"
+									>
+										Hear more from Hayashi
+									</label>
+									<div style={{ position: "relative" }}>
+										<div
+											id="Zc_SignupSuccess"
+											style={{
+												display: "none",
+												position: "absolute",
+												marginLeft: "4%",
+												width: "90%",
+												backgroundColor: "white",
+												padding: 3,
+												border: "3px solid rgb(194, 225, 154)",
+												marginTop: 10,
+												marginBottom: 10,
+												wordBreak: "break-all"
+											}}
+										>
+											<table width="100%" cellPadding={0} cellSpacing={0} border={0}>
+												<tbody>
+													<tr>
+														<td width="10%">
+															<img
+																className="successicon"
+																src="https://hqepw-zglp.maillist-manage.com/images/challangeiconenable.jpg"
+																align="absmiddle"
+															/>
+														</td>
+														<td>
+															<span
+																id="signupSuccessMsg"
+																style={{
+																	color: "rgb(73, 140, 132)",
+																	fontFamily: "sans-serif",
+																	fontSize: 14,
+																	wordBreak: "break-word"
+																}}
+															>
+																&nbsp;&nbsp;Thank you for Signing Up
+															</span>
+														</td>
+													</tr>
+												</tbody>
+											</table>
+										</div>
+									</div>
+									<form
+										method="POST"
+										id="zcampaignOptinForm"
+										style={{ margin: 0, width: "100%" }}
+										action="https://hqepw-zglp.maillist-manage.com/weboptin.zc"
+										target="_zcSignup"
+										className={styles.mailchimp}
+									>
+										<div
+											style={{
+												backgroundColor: "rgb(255, 235, 232)",
+												padding: 10,
+												color: "rgb(210, 0, 0)",
+												fontSize: 11,
+												margin: "20px 10px 0px",
+												border: "1px solid rgb(255, 217, 211)",
+												opacity: 1,
+												display: "none"
+											}}
+											id="errorMsgDiv"
+										>
+											Please correct the marked field(s) below.
+										</div>
+										<div className="SIGNUP_FLD">
+											<input
+												type="email"
+												placeholder="Enter e-mail"
+												changeitem="SIGNUP_FORM_FIELD"
+												name="CONTACT_EMAIL"
+												id="EMBED_FORM_EMAIL_LABEL"
+											/>
+
+											<button
+												type="submit"
+												name="SIGNUP_SUBMIT_BUTTON"
+												id="zcWebOptin"
+											>
+												&rarr;
+											</button>
+										</div>
+										<input type="hidden" id="fieldBorder" defaultValue="" />
+										<input
+											type="hidden"
+											id="submitType"
+											name="submitType"
+											defaultValue="optinCustomView"
+										/>
+										<input
+											type="hidden"
+											id="emailReportId"
+											name="emailReportId"
+											defaultValue=""
+										/>
+										<input
+											type="hidden"
+											id="formType"
+											name="formType"
+											defaultValue="QuickForm"
+										/>
+										<input
+											type="hidden"
+											name="zx"
+											id="cmpZuid"
+											defaultValue="12e1ad3ac"
+										/>
+										<input type="hidden" name="zcvers" defaultValue={3.0} />
+										<input
+											type="hidden"
+											name="oldListIds"
+											id="allCheckedListIds"
+											defaultValue=""
+										/>
+										<input
+											type="hidden"
+											id="mode"
+											name="mode"
+											defaultValue="OptinCreateView"
+										/>
+										<input
+											type="hidden"
+											id="zcld"
+											name="zcld"
+											defaultValue="1cfb060ce16235e5"
+										/>
+										<input
+											type="hidden"
+											id="zctd"
+											name="zctd"
+											defaultValue="1cfb060ce15fe411"
+										/>
+										<input type="hidden" id="document_domain" defaultValue="" />
+										<input
+											type="hidden"
+											id="zc_Url"
+											defaultValue="hqepw-zglp.maillist-manage.com"
+										/>
+										<input type="hidden" id="new_optin_response_in" defaultValue={0} />
+										<input
+											type="hidden"
+											id="duplicate_optin_response_in"
+											defaultValue={0}
+										/>
+										<input
+											type="hidden"
+											name="zc_trackCode"
+											id="zc_trackCode"
+											defaultValue="ZCFORMVIEW"
+										/>
+										<input
+											type="hidden"
+											id="zc_formIx"
+											name="zc_formIx"
+											defaultValue="3z5fb2122fba895524fb95f5eb7bc1e05e8d1492518627420aa5e5dd518c37d62a"
+										/>
+										<input type="hidden" id="viewFrom" defaultValue="URL_ACTION" />
+										<span style={{ display: "none" }} id="dt_CONTACT_EMAIL">
+											1,true,6,Contact Email,2
+										</span>
+									</form>
+								</div>
+							</div>
+						</div>
+						<img
+							src="https://hqepw-zglp.maillist-manage.com/images/spacer.gif"
+							id="refImage"
+							style={{ display: "none" }}
+						/>
+					</div>
+					<input
+						type="hidden"
+						id="signupFormType"
+						defaultValue="QuickForm_Horizontal"
+					/>
+					<div
+						id="zcOptinOverLay"
+						style={{
+							display: "none",
+							textAlign: "center",
+							backgroundColor: "rgb(0, 0, 0)",
+							opacity: "0.5",
+							zIndex: 100,
+							position: "fixed",
+							width: "100%",
+							top: 0,
+							left: 0,
+							height: 988
+						}}
+					/>
+					<div
+						id="zcOptinSuccessPopup"
+						style={{
+							display: "none",
+							zIndex: 9999,
+							width: 800,
+							height: "40%",
+							top: 84,
+							position: "fixed",
+							left: "26%",
+							backgroundColor: "#FFFFFF",
+							borderColor: "#E6E6E6",
+							borderStyle: "solid",
+							borderWidth: 1,
+							boxShadow: "0 1px 10px #424242",
+							padding: 35
+						}}
+					>
+						<span
+							style={{
+								position: "absolute",
+								top: "-16px",
+								right: "-14px",
+								zIndex: 99999,
+								cursor: "pointer"
+							}}
+							id="closeSuccess"
+						>
+							<img src="https://hqepw-zglp.maillist-manage.com/images/videoclose.png" />
+						</span>
+						<div id="zcOptinSuccessPanel" />
+					</div>
+					{/*Zoho Campaigns Web-Optin Form Ends Here*/}
+				</>
+
+
+
 				{/* @TODO :: Hook this up to mailchimp, waiting on client to set this up */}
-				<label htmlFor="newsletter">Hear more from Hayashi</label>
+				{/* <label htmlFor="newsletter">Hear more from Hayashi</label>
 				<form className={styles.mailchimp}>
-					<input id="newsletter" placeholder="Enter e-mail" type="email" />
+					<input id="newsletter" placeholder="Enter e-mail" type="email" / />
 					<button type="submit">&rarr;</button>
-				</form>
+				</form> /*/}
 			</div>
 
 			<Logo width={85} height={15} />
