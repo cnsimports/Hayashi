cc6447696422753f0897c9b2ddc638b24eb08f9f
Dominic Magnifico <dominicmagnifico@gmail.com>
2022-09-15 16:55:52 -0600

    Linting fixes.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,12 @@
 module.exports = {
 	reactStrictMode: true,
 	images: {
-		domains: ['source.unsplash.com', 'res.cloudinary.com', 'localhost', 'hayashiwhisky.herokuapp.com', 'hayashiwhisky.com'],
+		domains: [
+			'source.unsplash.com',
+			'res.cloudinary.com',
+			'localhost',
+			'hayashiwhisky.herokuapp.com',
+			'hayashiwhisky.com',
+		],
 	},
 };
