ec52193e12e48724def299d44bbdbd99f8567d94
Daniel Grosso <daniel@onadev.ar>
2022-12-15 11:54:49 -0300

    Fixed decoration line height

--- frontend/components/SplitContent/SplitContent.js
+++ frontend/components/SplitContent/SplitContent.js
@@ -1,19 +1,31 @@
 import { getStrapiMedia } from '@lib/media';
 import Image from 'next/future/image';
 import PropTypes from 'prop-types';
+import { useEffect, useRef } from 'react';
 import ReactMarkdown from 'react-markdown';
 
 import styles from './SplitContent.module.css';
 
 export const SplitContent = ({ image, topic, title, content, variant }) => {
-	console.log(styles);
+	const contentRef = useRef(null)
+	const contentWrapperRef = useRef(null)
+
+	useEffect(() => {
+		if (contentWrapperRef.current && contentRef.current) {
+			const heightDiff = contentRef.current.getBoundingClientRect().bottom - contentWrapperRef.current.getBoundingClientRect().bottom;
+			contentWrapperRef.current.style.setProperty('--height-diff', `${heightDiff}px`)
+		}
+	}, [])
+
 	return (
 		<div className={`${styles['split-content']} ${styles[variant.replaceAll('_', '-')]}`}>
-			<div className={styles.content}>
+			<div ref={contentRef} className={styles.content}>
 				<div className={styles['content-wrapper']}>
 					<h2 className={`${styles.topic} -uppercase`}>{topic}</h2>
 					<ReactMarkdown className={styles.title}>{title}</ReactMarkdown>
-					<ReactMarkdown className={styles.text}>{content}</ReactMarkdown>
+					<div ref={contentWrapperRef}>
+						<ReactMarkdown className={styles.text}>{content}</ReactMarkdown>
+					</div>
 				</div>
 			</div>
 			<Image
